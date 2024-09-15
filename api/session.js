81638ff022fd09c018c0e443cde3e0972025e3d5
eitan-que <eitanfeldman021@gmail.com>
2024-09-15 17:19:07 -0300

    0.76
    Back to 0.73

--- api/session.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/session', (req, res) => {
-    res.json({ isAuthenticated: !!req.session.userId });
-});
-
-module.exports = router;
